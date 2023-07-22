9549f5b817ee68d2ab9052a70b8c44ba11889e44
pierinho13 <pierinho13@gmail.com>
2023-07-22 23:57:27 +0200

    creating server

---
+++ first-server/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.9.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = "eu-west-1"
+  shared_credentials_files = ["/home/piero/.aws/credentials"]
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "my_server" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+}
