38e7d64f045b96191c7b90b6380309e3215f772b
pierinho13 <pierinho13@gmail.com>
2023-07-24 21:34:49 +0200

    load balancer

--- first-server/main.tf
+++ first-server/main.tf
@@ -12,11 +12,6 @@ provider "aws" {
   shared_credentials_files = ["/home/piero/.aws/credentials"]
 }
 
-
-data "aws_vpc" "default" {
-  default = true
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -33,22 +28,53 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "my_server" {
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet" "availabily_zone_a" {
+  availability_zone = "eu-west-1a"
+}
+
+data "aws_subnet" "availabily_zone_b" {
+  availability_zone = "eu-west-1b"
+}
+
+#-----------------------------------------
+# Define instancias
+#-----------------------------------------
+resource "aws_instance" "servidor_1" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnet.availabily_zone_a.id
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
   user_data              = <<-EOF
               #!/bin/bash
-              echo "hola mundo" > index.html
+              echo "hola mundo desde servidor 1" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
   tags = {
-    Name =  "server-1"
+    Name = "server-1"
+  }
+}
+
+resource "aws_instance" "servidor_2" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnet.availabily_zone_b.id
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  user_data              = <<-EOF
+              #!/bin/bash
+              echo "hola mundo desde servidor 2" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  tags = {
+    Name = "server-2"
   }
 }
 
 resource "aws_security_group" "my_security_group" {
-  name = "firewall_ingress"
+  name   = "firewall_ingress"
   vpc_id = data.aws_vpc.default.id
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
@@ -58,3 +84,50 @@ resource "aws_security_group" "my_security_group" {
     protocol    = "TCP"
   }
 }
+
+resource "aws_security_group" "my_security_group_load_balancer" {
+  name   = "firewall_ingress_load_balancer"
+  vpc_id = data.aws_vpc.default.id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "open port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+  }
+}
+
+resource "aws_lb" "load_balancer_app" {
+  load_balancer_type = "application"
+  name               = "load-balancer-app"
+  security_groups    = [aws_security_group.my_security_group_load_balancer.id]
+  subnets            = [data.aws_subnet.availabily_zone_b.id, data.aws_subnet.availabily_zone_a.id]
+
+}
+
+resource "aws_lb_target_group" "load_balancer_target_app" {
+  name = "load-balancer-target-app"
+  port = 80
+  vpc_id = data.aws_vpc.default.id
+  protocol = "HTTP"
+  health_check {
+    enabled = true
+    matcher = "200"
+    path = "/"
+    port = "8080"
+    protocol = "HTTP"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "attachment_servidor_1" {
+  target_group_arn = aws_lb_target_group.load_balancer_target_app.arn
+  target_id = aws_instance.servidor_1.id
+  port = 8080
+}
+
+resource "aws_lb_target_group_attachment" "attachment_servidor_2" {
+  target_group_arn = aws_lb_target_group.load_balancer_target_app.arn
+  target_id = aws_instance.servidor_2.id
+  port = 8080
+}
+
--- first-server/outputs.tf
+++ first-server/outputs.tf
@@ -1,12 +1,23 @@
-output "public_dns" {
-  value = aws_instance.my_server.public_dns
+output "public_dns_server_1" {
+  value = aws_instance.servidor_1.public_dns
+}
+output "public_dns_server2" {
+  value = aws_instance.servidor_2.public_dns
 }
 
-output "public_ip" {
-  value = aws_instance.my_server.public_ip
+output "public_ip_server_1" {
+  value = aws_instance.servidor_1.public_ip
+}
+output "public_ip_server_2" {
+  value = aws_instance.servidor_2.public_ip
 }
 
 output "my_app" {
-  value = "http://${aws_instance.my_server.public_dns}:8080"
+  value       = "http://${aws_instance.servidor_1.public_dns}:8080"
+  description = "public dns my app in my server"
+}
+
+output "my_app_2" {
+  value       = "http://${aws_instance.servidor_2.public_dns}:8080"
   description = "public dns my app in my server"
 }
